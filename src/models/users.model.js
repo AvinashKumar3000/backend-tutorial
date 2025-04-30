3537df5d0b8f511886c006a5202f6569e62f0b08
Avi <avinashkumar@mviiitech.com>
2025-04-30 11:26:10 +0530

    updated schema

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,38 +1,37 @@
 const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema({
-        username: String,
-        email: { 
-            type: String, 
-            required: true, 
-            unique: [true, "user with email already exists"],
-            trim: true,
-            lowercase: true, 
-            index: true
-        } ,
-        password: {
-            type: String,
-            required: true 
-        } ,
-        details: {
-            personal: {
-                name: { 
-                    firstName: String,
-                    lastName: String
-                },
-                dob: Date,
-                address: {
-                    city: String,
-                    state: String,
-                    country: String
-                }
-            },
-            skills: {
-                technical: [String],
-                soft:[String],
-            }
-        }
-    });
+  username: String,
+  email: {
+    type: String,
+    required: [true, "email is required "],
+    unique: [true, "user with email already exists"],
+    lowercase: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  details: {
+    personal: {
+      name: {
+        firstName: String,
+        lastName: String,
+      },
+      dob: Date,
+      address: {
+        city: String,
+        state: String,
+        nationality: String,
+      },
+    },
+    skills: {
+      technical: [String],
+      soft: [String],
+    },
+  },
+});
 
-const UsersModel = mongoose.model('users', usersSchema);
+const UsersModel = mongoose.model("users", usersSchema);
 module.exports = UsersModel;
