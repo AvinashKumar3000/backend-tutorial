0934789f1a0407d3181f78c1a8105418a6c07d2c
Avi <avinashkumar@mviiitech.com>
2025-05-02 13:41:27 +0530

    update otp logics

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -40,14 +40,17 @@ exports.requestOtp = async function (req, res) {
         const payload = req.body;
         const { status } = await usersService.isUserEmailExists(payload.email);
         if (!status) throw new Error('email not exists');
-        const otp = await otpService.genOtp(payload.email);
+        const { otp, _id } = await otpService.genOtp(payload.email);
         await emailService.sendOtp(payload.email, otp);
         res.status(200).json({
             status: true,
             message: 'otp sent to your email',
+            data: {
+                id: _id,
+            },
         });
     } catch (error) {
-        res.status(400).status({
+        res.status(400).json({
             status: false,
             message: error.message,
         });
@@ -58,12 +61,12 @@ exports.verifyOtp = async function (req, res) {
     try {
         const payload = req.body;
         await otpService.compare(payload.id, payload.otp);
-        res.status(200).status({
+        res.status(200).json({
             status: true,
             message: 'otp verification successful',
         });
     } catch (error) {
-        res.status(400).status({
+        res.status(400).json({
             status: false,
             message: error.message,
         });
