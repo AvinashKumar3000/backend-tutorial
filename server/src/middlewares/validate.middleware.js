0a5af0f1ea6ef5ad9de8b98807080dca01a4c9ca
Avi <avinashkumar@mviiitech.com>
2025-05-04 21:16:19 +0530

    refactor: update validateMiddleware to accept schema only and retrieve payload from request body

--- server/src/middlewares/validate.middleware.js
+++ server/src/middlewares/validate.middleware.js
@@ -1,6 +1,7 @@
-function validateMiddleware (schema, payload) {
-    const { error } = schema.validate(payload);
+function validateMiddleware (schema) {
     function middlewareHandler(req,res,next) {
+        const payload = req.body;
+        const { error } = schema.validate(payload);
         if(error) {
             res.send(400).json({
                 status: false,
