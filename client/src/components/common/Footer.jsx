ee7a8cdf542cc97a7b9639c73e90bbf802956e60
Avi <avinashkumar@mviiitech.com>
2025-05-05 12:05:13 +0530

    sample

---
+++ client/src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-black text-white'>
+        <h1>footer</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus facilis distinctio quo debitis nemo nesciunt ducimus accusantium sapiente magni quia! Molestias aliquid dolorum alias veritatis? Facilis officiis atque corporis corrupti?</p>
+    </div>
+  )
+}
